15de53fe75e8f9eca8d1849e9fbf94f5317c44e6
PyScolok <doc.dr2011@mail.ru>
2021-01-20 22:09:32 +0300

    Created and registered  models.

--- polling_system/polls/admin.py
+++ polling_system/polls/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Poll, Question, Choice
+
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'start_date', 'finish_date')
+    search_fields = ('name', 'author')
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('poll', 'question_text', 'question_type')
+    search_fields = ('poll', 'question_type')
+    list_filter = ('question_type', )
+    save_as = True
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'choice_text')
+    search_fields = ('choice_text', )
+    save_as = True
--- polling_system/polls/models.py
+++ polling_system/polls/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Poll(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='polls')
+    description = models.TextField()
+    start_date = models.DateField(auto_now_add=True)
+    finish_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    QUESTION_TYPES = (
+        ('free', 'free answer'),
+        ('one', 'one answer'),
+        ('many', 'several answers'),
+    )
+
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name='questions')
+    question_text = models.CharField(max_length=255)
+    question_type = models.CharField(max_length=255, choices=QUESTION_TYPES)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+    choice_text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Vote(models.Model):
+    respondent_id = models.PositiveIntegerField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='votes')
+    answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.answer
